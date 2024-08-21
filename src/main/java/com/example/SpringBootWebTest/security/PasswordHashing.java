dbeacd150de05ba5fbae7b0f7e36a21fd664c097
Ertugrul Eryildiz <ertugruleryldz@gmail.com>
2024-08-21 10:47:40 -0400

    Removes @Configuration bean from the PasswordHashing class.

--- src/main/java/com/example/SpringBootWebTest/security/PasswordHashing.java
+++ src/main/java/com/example/SpringBootWebTest/security/PasswordHashing.java
@@ -1,9 +1,8 @@
 package com.example.SpringBootWebTest.security;
 
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCrypt;
 
-@Configuration
+
 public class PasswordHashing {
 	public static String hashPassword(String password) {
 		return BCrypt.hashpw(password, BCrypt.gensalt());
